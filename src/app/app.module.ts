68f91ea6fc5af6da725cbd990b6e7718f99ab4fe
grafox <g_fox_fts@yahoo.com>
2017-08-13 19:44:58 +0200

    ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
+import { AngularFireAuthModule, AngularFireAuth } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+    apiKey: "AIzaSyBx17p57GoqgagclJzBV8PujkcG_MoKxMI",
+    authDomain: "labeleco-723a7.firebaseapp.com",
+    databaseURL: "https://labeleco-723a7.firebaseio.com",
+    projectId: "labeleco-723a7",
+    storageBucket: "labeleco-723a7.appspot.com",
+    messagingSenderId: "166154079476"
+};
+
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
